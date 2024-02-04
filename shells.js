08d2b211cd6371cd4511b2a04f4879b86ceb5682
fleri2016 <lflerianos@gmail.com>
2024-02-04 20:47:55 +0200

    Files Upload

---
+++ shells.js
@@ -0,0 +1,461 @@
+// A string containing the shells of each element.
+var str =`(K)1
+(K)2
+(K)2 (L)1
+(K)2 (L)2
+(K)2 (L)3
+(K)2 (L)4
+(K)2 (L)5
+(K)2 (L)6
+(K)2 (L)7
+(K)2 (L)8
+(K)2 (L)8 (M)1
+(K)2 (L)8 (M)2
+(K)2 (L)8 (M)3
+(K)2 (L)8 (M)4
+(K)2 (L)8 (M)5
+(K)2 (L)8 (M)6
+(K)2 (L)8 (M)7
+(K)2 (L)8 (M)8
+(K)2 (L)8 (M)8 (N)1
+(K)2 (L)8 (M)8 (N)2
+(K)2 (L)8 (M)9 (N)2
+(K)2 (L)8 (M)10 (N)2
+(K)2 (L)8 (M)11 (N)2
+(K)2 (L)8 (M)13 (N)1
+(K)2 (L)8 (M)13 (N)2
+(K)2 (L)8 (M)14 (N)2
+(K)2 (L)8 (M)15 (N)2
+(K)2 (L)8 (M)16 (N)2
+(K)2 (L)8 (M)18 (N)1
+(K)2 (L)8 (M)18 (N)2
+(K)2 (L)8 (M)18 (N)3
+(K)2 (L)8 (M)18 (N)4
+(K)2 (L)8 (M)18 (N)5
+(K)2 (L)8 (M)18 (N)6
+(K)2 (L)8 (M)18 (N)7
+(K)2 (L)8 (M)18 (N)8
+(K)2 (L)8 (M)18 (N)8 (O)1
+(K)2 (L)8 (M)18 (N)8 (O)2
+(K)2 (L)8 (M)18 (N)9 (O)2
+(K)2 (L)8 (M)18 (N)10 (O)2
+(K)2 (L)8 (M)18 (N)12 (O)1
+(K)2 (L)8 (M)18 (N)13 (O)1
+(K)2 (L)8 (M)18 (N)13 (O)2
+(K)2 (L)8 (M)18 (N)15 (O)1
+(K)2 (L)8 (M)18 (N)16 (O)1
+(K)2 (L)8 (M)18 (N)18
+(K)2 (L)8 (M)18 (N)18 (O)1
+(K)2 (L)8 (M)18 (N)18 (O)2
+(K)2 (L)8 (M)18 (N)18 (O)3
+(K)2 (L)8 (M)18 (N)18 (O)4
+(K)2 (L)8 (M)18 (N)18 (O)5
+(K)2 (L)8 (M)18 (N)18 (O)6
+(K)2 (L)8 (M)18 (N)18 (O)7
+(K)2 (L)8 (M)18 (N)18 (O)8
+(K)2 (L)8 (M)18 (N)18 (O)8 (P)1
+(K)2 (L)8 (M)18 (N)18 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)18 (O)9 (P)2
+(K)2 (L)8 (M)18 (N)19 (O)9 (P)2
+(K)2 (L)8 (M)18 (N)21 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)22 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)23 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)24 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)25 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)25 (O)9 (P)2
+(K)2 (L)8 (M)18 (N)27 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)28 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)29 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)30 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)31 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)8 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)9 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)10 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)11 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)12 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)13 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)14 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)15 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)17 (P)1
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)1
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)2
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)3
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)4
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)5
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)6
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)7
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)8
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)8 (Q)1
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)9 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)18 (P)10 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)20 (P)9 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)21 (P)9 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)22 (P)9 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)24 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)25 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)25 (P)9 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)27 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)28 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)29 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)30 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)31 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)8 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)8 (Q)3
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)10 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)11 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)12 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)13 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)14 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)15 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)16 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)17 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)2
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)3
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)4
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)5
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)6
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)7
+(K)2 (L)8 (M)18 (N)32 (O)32 (P)18 (Q)8`;
+
+const shells = str.split('\n'); //Convert string to array
+
+//Array of all elements 
+const elements = [
+  'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Nitrogen', 'Oxygen',
+  'Fluorine', 'Neon', 'Sodium', 'Magnesium', 'Aluminum', 'Silicon', 'Phosphorus', 'Sulfur',
+  'Chlorine', 'Argon', 'Potassium', 'Calcium', 'Scandium', 'Titanium', 'Vanadium', 'Chromium',
+  'Manganese', 'Iron', 'Nickel', 'Cobalt', 'Copper', 'Zinc', 'Gallium', 'Germanium', 'Arsenic',
+  'Selenium', 'Bromine', 'Krypton', 'Rubidium', 'Strontium', 'Yttrium', 'Zirconium', 'Niobium',
+  'Molybdenum', 'Technetium', 'Ruthenium', 'Rhodium', 'Palladium', 'Silver', 'Cadmium', 'Indium',
+  'Tin', 'Antimony', 'Tellurium', 'Iodine', 'Xenon', 'Cesium', 'Barium', 'Lanthanum', 'Cerium',
+  'Praseodymium', 'Neodymium', 'Promethium', 'Samarium', 'Europium', 'Gadolinium', 'Terbium', 'Dysprosium',
+  'Holmium', 'Erbium', 'Thulium', 'Ytterbium', 'Lutetium', 'Hafnium', 'Tantalum', 'Tungsten', 'Rhenium',
+  'Osmium', 'Iridium', 'Platinum', 'Gold', 'Mercury', 'Thallium', 'Lead', 'Bismuth', 'Polonium', 'Astatine',
+  'Radon', 'Francium', 'Radium', 'Actinium', 'Thorium', 'Protactinium', 'Uranium', 'Neptunium', 'Plutonium',
+  'Americium', 'Curium', 'Berkelium', 'Californium', 'Einsteinium', 'Fermium', 'Mendelevium', 'Nobelium', 'Lawrencium',
+  'Rutherfordium', 'Dubnium', 'Seaborgium', 'Bohrium', 'Hassium', 'Meitnerium', 'Darmstadtium', 'Roentgenium', 'Copernicium',
+  'Nihonium', 'Flerovium', 'Moscovium', 'Livermorium', 'Tennessine', 'Oganesson'
+];
+
+// Array of symbols for the elements
+const symbols = [
+  'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O',
+  'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S',
+  'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr',
+  'Mn', 'Fe', 'Ni', 'Co', 'Cu', 'Zn', 'Ga', 'Ge', 'As',
+  'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb',
+  'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In',
+  'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce',
+  'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy',
+  'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re',
+  'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At',
+  'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu',
+  'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+  'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn',
+  'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'
+];
+
+// String containing all electrong configs of elements
+str = `1s¹
+1s²
+1s²2s¹
+1s²2s²
+1s²2s²2p¹
+1s²2s²2p²
+1s²2s²2p³
+1s²2s²2p⁴
+1s²2s²2p⁵
+1s²2s²2p⁶
+1s²2s²2p⁶3s¹
+1s²2s²2p⁶3s²
+1s²2s²2p⁶3s²3p¹
+1s²2s²2p⁶3s²3p²
+1s²2s²2p⁶3s²3p³
+1s²2s²2p⁶3s²3p⁴
+1s²2s²2p⁶3s²3p⁵
+1s²2s²2p⁶3s²3p⁶
+1s²2s²2p⁶3s²3p⁶4s¹
+1s²2s²2p⁶3s²3p⁶4s²
+1s²2s²2p⁶3s²3p⁶3d¹4s²
+1s²2s²2p⁶3s²3p⁶3d²4s²
+1s²2s²2p⁶3s²3p⁶3d³4s²
+1s²2s²2p⁶3s²3p⁶3d⁵4s¹
+1s²2s²2p⁶3s²3p⁶3d⁵4s²
+1s²2s²2p⁶3s²3p⁶3d⁶4s²
+1s²2s²2p⁶3s²3p⁶3d⁷4s²
+1s²2s²2p⁶3s²3p⁶3d⁸4s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p³
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁴
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁵
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶5s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹5s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d²5s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d⁴5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d⁵5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d⁵5s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d⁷5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d⁸5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p³
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁴
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁵
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁶
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁶6s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰5s²5p⁶5d¹6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹5s²5p⁶5d¹6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f³5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁴5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁵5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁶5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁷5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁷5s²5p⁶5d¹6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f⁹5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁰5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹¹5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹²5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹³5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d²6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d³6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d⁴6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d⁵6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d⁶6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d⁷6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d⁹6s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p³
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁴
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁵
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁶
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁶7s¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁶6d¹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰6s²6p⁶6d²7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f²6s²6p⁶6d¹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f³6s²6p⁶6d¹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f⁴6s²6p⁶6d¹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f⁶6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f⁷6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f⁷6s²6p⁶6d¹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f⁹6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁰6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹16s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹²6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹³6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶7s²7p¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d²7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d³7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁴7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁵7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁶7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁷7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁸7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d⁹7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p¹
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p²
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p³
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p⁴
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p⁵
+1s²2s²2p⁶3s²3p⁶3d¹⁰4s²4p⁶4d¹⁰4f¹⁴5s²5p⁶5d¹⁰5f¹⁴6s²6p⁶6d¹⁰7s²7p⁶`
+
+const configFull = str.split('\n'); // Convert string to array
+
+// String containing the shortened electron configuration of elements, by using Noble Gasses.
+str = `1s¹
+1s²
+[He]2s¹
+[He]2s²
+[He]2s²2p¹
+[He]2s²2p²
+[He]2s²2p³
+[He]2s²2p⁴
+[He]2s²2p⁵
+[He]2s²2p⁶
+[Ne]3s¹
+[Ne]3s²
+[Ne]3s²3p¹
+[Ne]3s²3p²
+[Ne]3s²3p³
+[Ne]3s²3p⁴
+[Ne]3s²3p⁵
+[Ne]3s²3p⁶
+[Ar]4s¹
+[Ar]4s²
+[Ar]3d¹4s²
+[Ar]3d²4s²
+[Ar]3d³4s²
+[Ar]3d⁵4s¹
+[Ar]3d⁵4s²
+[Ar]3d⁶4s²
+[Ar]3d⁷4s²
+[Ar]3d⁸4s²
+[Ar]3d¹⁰4s¹
+[Ar]3d¹⁰4s²
+[Ar]3d¹⁰4s²4p¹
+[Ar]3d¹⁰4s²4p²
+[Ar]3d¹⁰4s²4p³
+[Ar]3d¹⁰4s²4p⁴
+[Ar]3d¹⁰4s²4p⁵
+[Ar]3d¹⁰4s²4p⁶
+[Kr]5s¹
+[Kr]5s²
+[Kr]4d¹5s²
+[Kr]4d²5s²
+[Kr]4d⁴5s¹
+[Kr]4d⁵5s¹
+[Kr]4d⁵5s²
+[Kr]4d⁷5s¹
+[Kr]4d⁸5s¹
+[Kr]4d¹⁰
+[Kr]4d¹⁰5s¹
+[Kr]4d¹⁰5s²
+[Kr]4d¹⁰5s²5p¹
+[Kr]4d¹⁰5s²5p²
+[Kr]4d¹⁰5s²5p³
+[Kr]4d¹⁰5s²5p⁴
+[Kr]4d¹⁰5s²5p⁵
+[Kr]4d¹⁰5s²5p⁶
+[Xe]6s¹
+[Xe]6s²
+[Xe]5d¹6s²
+[Xe]4f¹5d¹6s²
+[Xe]4f³6s²
+[Xe]4f⁴6s²
+[Xe]4f⁵6s²
+[Xe]4f⁶6s²
+[Xe]4f⁷6s²
+[Xe]4f⁷5d¹6s²
+[Xe]4f⁹6s²
+[Xe]4f¹⁰6s²
+[Xe]4f¹¹6s²
+[Xe]4f¹²6s²
+[Xe]4f¹³6s²
+[Xe]4f¹⁴6s²
+[Xe]4f¹⁴5d¹6s²
+[Xe]4f¹⁴5d²6s²
+[Xe]4f¹⁴5d³6s²
+[Xe]4f¹⁴5d⁴6s²
+[Xe]4f¹⁴5d⁵6s²
+[Xe]4f¹⁴5d⁶6s²
+[Xe]4f¹⁴5d⁷6s²
+[Xe]4f¹⁴5d⁹6s¹
+[Xe]4f¹⁴5d¹⁰6s¹
+[Xe]4f¹⁴5d¹⁰6s²
+[Xe]4f¹⁴5d¹⁰6s²6p¹
+[Xe]4f¹⁴5d¹⁰6s²6p²
+[Xe]4f¹⁴5d¹⁰6s²6p³
+[Xe]4f¹⁴5d¹⁰6s²6p⁴
+[Xe]4f¹⁴5d¹⁰6s²6p⁵
+[Xe]4f¹⁴5d¹⁰6s²6p⁶
+[Rn]7s¹
+[Rn]7s²
+[Rn]6d¹7s²
+[Rn]6d²7s²
+[Rn]5f²6d¹7s²
+[Rn]5f³6d¹7s²
+[Rn]5f⁴6d¹7s²
+[Rn]5f⁶7s²
+[Rn]5f⁷7s²
+[Rn]5f⁷6d¹7s²
+[Rn]5f⁹7s²
+[Rn]5f¹⁰7s²
+[Rn]5f¹17s²
+[Rn]5f¹²7s²
+[Rn]5f¹³7s²
+[Rn]5f¹⁴7s²
+[Rn]5f¹⁴7s²7p¹
+[Rn]5f¹⁴6d²7s²
+[Rn]5f¹⁴6d³7s²
+[Rn]5f¹⁴6d⁴7s²
+[Rn]5f¹⁴6d⁵7s²
+[Rn]5f¹⁴6d⁶7s²
+[Rn]5f¹⁴6d⁷7s²
+[Rn]5f¹⁴6d⁸7s²
+[Rn]5f¹⁴6d⁹7s²
+[Rn]5f¹⁴6d¹⁰7s²
+[Rn]5f¹⁴6d¹⁰7s²7p¹
+[Rn]5f¹⁴6d¹⁰7s²7p²
+[Rn]5f¹⁴6d¹⁰7s²7p³
+[Rn]5f¹⁴6d¹⁰7s²7p⁴
+[Rn]5f¹⁴6d¹⁰7s²7p⁵
+[Rn]5f¹⁴6d¹⁰7s²7p⁶`;
+
+const configShort = str.split('\n'); // Convert string to array
+
+// The following function will create a matrix with 5 columns and 118 rows, combining all the above data into one table:
+
+function createMatrix() 
+{
+  const electronConfig = [];
+  
+  for (let i = 0; i < 118; i++){
+    electronConfig[i] = [];
+    electronConfig[i][0] = elements[i];
+    electronConfig[i][1] = symbols[i];
+    electronConfig[i][2] = shells[i];
+    electronConfig[i][3] = configFull[i];
+    electronConfig[i][4] = configShort[i];
+  }
+
+  return electronConfig;
+}
+
+// The following function receives an argument, which can be a String or Number. It then runs the createMatrix() function, and uses the argument it receives to find the element searched, and returns its configuration.
+
+function getConfig(input)
+{
+  const matrix = createMatrix();
+  if(isNaN(parseFloat(input))){ // Checks for search by atomic number
+    if(input.length < 3) { // Checks for search by Symbol
+      for(let i = 0; i<118 ; i++){
+        if(input.toUpperCase() === matrix[i][1].toUpperCase()){
+          input = parseInt(i+1);
+          break;
+        }
+      }
+    } else {
+      for(let i = 0; i<118 ; i++){ //Checks for search by Element Name
+        if(input.toUpperCase() === matrix[i][0].toUpperCase()){
+          input = parseInt(i+1);
+          break;
+        }
+      }
+    }
+  } else {
+    input = parseInt(input);
+    if(input % 1 !== 0) {input = Math.floor(input)}; // Response Validation: checks for decimals
+  } 
+  
+  if(input < 1 || input > 118 || typeof(input) == 'string'){ // Response Validation: Result?
+    let response = 'nf';
+    console.log(typeof(input));
+    document.getElementById('out').innerHTML = '<br><br><hr><b><p style="color:#CC2017;">We searched the Entire Periodic Table, but could not find the element you searched for!</p></b>'
+    return response;
+  }
+
+  let response = 'Electron Configuration for '  + matrix[input-1][0] + ' (' + matrix[input-1][1] + ')\n \n Shells Distribution: ' + matrix[input-1][2] + '\nElectron Configuration (full): ' + matrix[input-1][3] + '\nElectron Configuration (shortened): ' +  matrix[input-1][4]; 
+  // Generates Response
+
+  // Prints response
+  document.getElementById('out').innerHTML = '<br><br><hr><b>Electron Configuration for '  + matrix[input-1][0] + ' (' + matrix[input-1][1] + ')</b><br><br><u>Shells Distribution:</u> ' + matrix[input-1][2] + '<br><u>Electron Configuration (full):</u> ' + matrix[input-1][3] + '<br><u>Electron Configuration (shortened):</u> ' +  matrix[input-1][4]; 
+  
+  return response;
+}
